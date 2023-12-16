ea6e1fde075b5367ce574463f7ad521b9d0a76b1
spring-user <casbah@mail.ru>
2023-12-16 19:38:31 +0300

    Adding validation rules to the forms

--- src/main/java/com/sabal/tacocloud/controllers/DesignTacoController.java
+++ src/main/java/com/sabal/tacocloud/controllers/DesignTacoController.java
@@ -4,9 +4,11 @@ import com.sabal.tacocloud.entities.Ingredient;
 import com.sabal.tacocloud.entities.Taco;
 import com.sabal.tacocloud.entities.TacoOrder;
 import com.sabal.tacocloud.entities.Type;
+import jakarta.validation.Valid;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Arrays;
@@ -58,7 +60,10 @@ public class DesignTacoController {
     }
 
     @PostMapping
-    public String processTaco(Taco taco, @ModelAttribute TacoOrder tacoOrder) {
+    public String processTaco(@Valid Taco taco, Errors errors, @ModelAttribute TacoOrder tacoOrder) {
+        if(errors.hasErrors()) {
+            return "design";
+        }
         tacoOrder.addTaco(taco);
         log.info("Processing Taco: {}", taco);
 
