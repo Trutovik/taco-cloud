81bde59bdf36722db03f4ed6ea8514ec9a79c76a
spring-user <casbah@mail.ru>
2023-12-16 18:39:46 +0300

    Processing Form

--- src/main/java/com/sabal/tacocloud/entities/TacoOrder.java
+++ src/main/java/com/sabal/tacocloud/entities/TacoOrder.java
@@ -15,7 +15,7 @@ public class TacoOrder {
     private String deliveryZip;
     private String ccNumber;
     private String ccExpiration;
-    private String ccCW;
+    private String ccCVV;
 
     private List<Taco> tacos = new ArrayList<>();
 
